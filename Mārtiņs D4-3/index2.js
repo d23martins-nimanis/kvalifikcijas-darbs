7b3cd675bef9495dff4410ff4122221051e00b57
Mārtiņš Nīmanis <56114897+d23martins-nimanis@users.noreply.github.com>
2022-06-09 14:05:26 +0300

    Add files via upload

--- Mārtiņs D4-3/index2.js
+++ Mārtiņs D4-3/index2.js
@@ -9,13 +9,19 @@ const FRICTION = 1;
 const TRIANGLE_SIZE = 30; // triangle highgt
 const TRIANGLE_ACCEL = 5; // acceleration
 const TURN_SPEED = 360;
+const FONT_SIZE = 60;
+const STARTING_LIFES = 5;
 //temporay:
 const CIRKLE_SIZE = 20;
 const CIRKLE_SPD = 30;
-const diff = 10;
+const DIFF = 10;
 const DIFF_STEP = 1
+const SQUARE_SIZE = CIRKLE_SIZE;
+const SQUARE_SPD = CIRKLE_SPD;
 
-var cirkles , score, scoreHigh, triangle, text, textAlpha;
+var cirkles , score, scoreHigh, triangle, text, textAlpha, bounce, as, squaresLeft, cirklesLeft, boun, lifes;
+
+var lifes = STARTING_LIFES;
 
         // set up triangle/player
         var triangle = {
@@ -34,6 +40,7 @@ var cirkles , score, scoreHigh, triangle, text, textAlpha;
         //event handlers
         document.addEventListener("keydown", Down);
         document.addEventListener("keyup", Up);
+				var boun = 0
 
         //game loop
         setInterval(update, 1000 / FPS);
@@ -66,11 +73,9 @@ var cirkles , score, scoreHigh, triangle, text, textAlpha;
             }
         }
 
-				spawnCirkle();
-
 				function spawnCirkle() {
         	  cirkles = [];
-          	for (var i = 0; i < DIFF_STEP + diff; i++) {
+          	for (var i = 0; i < score + DIFF /2; i++) {
             	  do {
              	     x = Math.floor(Math.random() * canv.width);
              	     y = Math.floor(Math.random() * canv.height);
@@ -79,15 +84,24 @@ var cirkles , score, scoreHigh, triangle, text, textAlpha;
 						}
 				}
 
+				function spawnSquare() {
+        	  squares = [];
+          	for (var i = 0; i < score + DIFF / 2; i++) {
+            	  do {
+             	     x = Math.floor(Math.random() * canv.width);
+             	     y = Math.floor(Math.random() * canv.height);
+          	    } while (distBetweenPoints(triangle.x, triangle.y, x, y) < SQUARE_SIZE * 2 + triangle.r);
+    	         	squares.push(newSquare(x, y, Math.ceil(SQUARE_SIZE / 2)));
+						}
+				}
+
 				function destroyCirkle(index) {
-						//score += 1;
-						// check high score
+						score += 1;
             //if (score > scoreHigh) {
             //    scoreHigh = score;
             //    localStorage.setItem(SAVE_KEY_SCORE, scoreHigh);
             //}
-            // destroy cirkle
-            cirkles.splice(index, 1);
+            //cirkles.splice(index, 1);
 				}
 
 				function distBetweenPoints(x1, y1, x2, y2) {
@@ -95,11 +109,12 @@ var cirkles , score, scoreHigh, triangle, text, textAlpha;
 				}
 
 				function newCirkle(x, y, r) {
-    				var diffMult = 1 + 0.1 * diff;
+    				var diffMult = 1 + 0.1 * DIFF;
+						console.log(cirkles.length);
     				var cirkle = {
     						x: x,
         				y: y,
-			     	   xv: Math.random() * CIRKLE_SPD * diffMult / FPS * (Math.random() < 0.5 ? 1 : -1),
+			     	    xv: Math.random() * CIRKLE_SPD * diffMult / FPS * (Math.random() < 0.5 ? 1 : -1),
 			      	  yv: Math.random() * CIRKLE_SPD * diffMult / FPS * (Math.random() < 0.5 ? 1 : -1),
 			      	  a: Math.random() * Math.PI * 2, // in radians
 			      	  r: r,
@@ -107,22 +122,78 @@ var cirkles , score, scoreHigh, triangle, text, textAlpha;
     				return cirkle;
 				}
 
+				function newSquare(x, y, r) {
+    				var diffMult = 1 + 0.1 * DIFF;
+    				var square = {
+    						x: x,
+        				y: y,
+			     	    xv: Math.random() * SQUARE_SPD * diffMult / FPS * (Math.random() < 0.5 ? 1 : -1),
+			      	  yv: Math.random() * SQUARE_SPD * diffMult / FPS * (Math.random() < 0.5 ? 1 : -1),
+			      	  a: Math.random() * Math.PI * 2, // in radians
+			      	  r: r,
+    				};
+    				return square;
+				}
+
+				function hit(){
+					lifes -= 1;
+				}
+
+				function bounce(x, y){
+					var sx, sy;
+					sx = x;
+					sy = y;
+					boun = 10;
+					console.log('bounce');
+
+					if (Math.abs(triangle.x - sx)> 10) {
+							triangle.accel.x = -triangle.accel.x * 1.2;
+					} else if (Math.abs(triangle.xy- sy)> 10) {
+							triangle.accel.y = -triangle.accel.y * 1.2;
+					} else {
+							triangle.accel.x = -triangle.accel.x * 1.2;
+							triangle.accel.y = -triangle.accel.y * 1.2;
+					}
+				}
+
+start();
+
+function start(){
+	score = 0;
+	spawnCirkle();
+	spawnSquare();
+}
+
         function update() {
             // bacground
             ctx.fillStyle = 'rgba(0,0,0,0.1)';
             ctx.fillRect(0, 0, canv.width, canv.height);
 
+						if (boun > 1){
+							boun -= 1;
+						}
+					
+						//zīmē punktus
+						ctx.textAlign = "right"
+						ctx.textBaseline = "middle"
+						ctx.fillStyle = "white"
+						ctx.font = FONT_SIZE + "Kdam Thmor Pro";
+						ctx.fillText("LIFES = " + lifes, TRIANGLE_SIZE * 2.5, TRIANGLE_SIZE);
+						ctx.fillText(score, canv.width - TRIANGLE_SIZE, TRIANGLE_SIZE);
+					
             // acceleration
             if (triangle.accelerating) {
                 triangle.accel.x += TRIANGLE_ACCEL * Math.cos(triangle.a) / FPS;
                 triangle.accel.y -= TRIANGLE_ACCEL * Math.sin(triangle.a) / FPS;
+								//console.clear()
+								//console.log(Math.sin(triangle.a) / FPS);
 
 						//friction
             } else {
                 triangle.accel.x -= FRICTION * triangle.accel.x / FPS;
                 triangle.accel.y -= FRICTION * triangle.accel.y / FPS;
             }
-
+					
             // draw the triangle
 						ctx.strokeStyle = 'white';
             ctx.lineWidth = TRIANGLE_SIZE / 20;
@@ -141,7 +212,6 @@ var cirkles , score, scoreHigh, triangle, text, textAlpha;
             );
             ctx.closePath();
 						ctx.stroke();
-						//ctx.fill();
 
             // triangle's movement
             triangle.a += triangle.rot;
@@ -176,7 +246,7 @@ var cirkles , score, scoreHigh, triangle, text, textAlpha;
                 ctx.arc(x, y, r, 0, Math.PI * 2, false);
                 ctx.stroke();
 						}
-
+					
 						//move cirkle
 						for (var i = 0; i < cirkles.length; i++) {
                 cirkles[i].x += cirkles[i].xv;
@@ -194,11 +264,87 @@ var cirkles , score, scoreHigh, triangle, text, textAlpha;
                     cirkles[i].y = 0 - cirkles[i].r
                 }
 						}
-						
+
+						//papildina riņķus
+						if (cirkles.length < score + DIFF){
+								console.log("lol cirkle");
+										do {
+             	     			x = Math.floor(Math.random() * canv.width);
+             	     			y = Math.floor(Math.random() * canv.height);
+          	    		} while (distBetweenPoints(triangle.x, triangle.y, x, y) < CIRKLE_SIZE * 2 + triangle.r);
+    	         			cirkles.push(newCirkle(x, y, Math.ceil(CIRKLE_SIZE / 2)));
+						}
+
+						//papildina kvadrātus
+						if (squares.length < score + DIFF){
+								console.log("lol square");
+										do {
+												x = Math.floor(Math.random() * canv.width);
+             	    			y = Math.floor(Math.random() * canv.height);
+          	    		} while (distBetweenPoints(triangle.x, triangle.y, x, y) < SQUARE_SIZE * 2 + triangle.r);
+    	         			squares.push(newSquare(x, y, Math.ceil(SQUARE_SIZE / 2)));
+						}
+
+					
+						//ja noķer riņķi
 						for (var i = 0; i < cirkles.length; i++) {
     						if (distBetweenPoints(triangle .x, triangle .y, cirkles[i].x, cirkles[i].y) < triangle .r + cirkles[i].r) {
         				destroyCirkle(i);
+								cirkles.splice(i, 1);
+								console.log(cirkles.length);									
         				break;
 								}
-						}            
-}
+						}
+						
+						//draw squares
+            var a, r, x, y;
+            for (var i = 0; i < squares.length; i++) {
+                ctx.strokeStyle = "slategrey";
+                ctx.lineWidth = TRIANGLE_SIZE / 20;
+
+                a = squares[i].a;
+                r = squares[i].r;
+                x = squares[i].x;
+                y = squares[i].y;
+
+								ctx.beginPath();
+                ctx.strokeRect(x - TRIANGLE_SIZE/2, y - TRIANGLE_SIZE/2, TRIANGLE_SIZE, TRIANGLE_SIZE)
+								ctx.strokeStyle = "red";
+                ctx.lineWidth = TRIANGLE_SIZE / 40;		
+							
+								ctx.beginPath();
+                ctx.arc(x, y, r/1.5, 0, Math.PI * 2, false);
+                ctx.stroke();
+						}
+					
+            // move square
+						for (var i = 0; i < squares.length; i++) {
+                squares[i].x += squares[i].xv;
+                squares[i].y += squares[i].yv;
+									
+                // handle square edge of screen
+                if (squares[i].x < 0 - squares[i].r) {
+                    squares[i].x = canv.width + squares[i].r;
+                } else if (squares[i].x > canv.width + squares[i].r) {
+                    squares[i].x = 0 - squares[i].r
+                }
+                if (squares[i].y < 0 - squares[i].r) {
+                    squares[i].y = canv.height + squares[i].r;
+                } else if (squares[i].y > canv.height + squares[i].r) {
+                    squares[i].y = 0 - squares[i].r
+                }
+						}
+						
+						for (var i = 0; i < squares.length; i++) {
+    						if (distBetweenPoints(triangle .x, triangle .y, squares[i].x, squares[i].y) < triangle .r + squares[i].r) {
+
+								if (boun <= 1){
+									bounce(squares[i].x, squares[i]);
+									squares.splice(i, 1);
+        					hit();
+								}
+        				break;
+								}
+						}
+				}
+
